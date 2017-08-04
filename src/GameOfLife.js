c7bbecdc2a87375697c06bcd91f1b1461bf1cc39
milesillsley <milesillsley@hotmail.co.uk>
2017-08-04 19:42:27 +0100

    Test passing for first rule: death by solitude

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -8,22 +8,23 @@ var GameOfLife = function(grid) {
 GameOfLife.prototype.progress = function() {
   for( var x=0; x<this._grid.length; x++ ) {
     for( var y=0; y<this._grid.length; y++ ) {
-      this.checkCondition(x,y, this._grid);
+      if (this._grid[x][y] === 1 && this.checkCondition(x,y) < 2) { this._futureGrid[x][y] = 0; }
     }
   }
+  this._grid = this._futureGrid;
 };
 
-GameOfLife.prototype.checkCondition = function(x,y, grid) {
+GameOfLife.prototype.checkCondition = function(x,y) {
   var company = 0;
   for( var a=-1; a<2; a++ ) {
     for( var b=-1; b<2; b++ ) {
       try {
         if (this._grid[x+a][y+b]) {
-          if ( this._grid[x+a][y+b] === 1 ) { company++ };
+          if ( this._grid[x+a][y+b] === 1 ) { company++; }
         }
-      } catch(error) { console.log(error); }
+      } catch(error) { }
     }
   }
-  if (this._grid[x][y] === 1) {company--};
+  if (this._grid[x][y] === 1) {company--;}
   return company;
 };
